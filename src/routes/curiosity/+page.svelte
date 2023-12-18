21c9a96de34c47fa77a2274c2738d487b09eb2b6
Adrian Matsumoto <adrianlimws@gmail.com>
2023-12-18 15:36:00 +1300

    modified search feature

--- src/routes/curiosity/+page.svelte
+++ src/routes/curiosity/+page.svelte
@@ -47,24 +47,34 @@
 
 <div class="navbar bg-slate-700 text-white">
 	<div class="navbar-start">
-		
 		SOL: {sol}
 	</div>
 	<div class="navbar-center">
 		<div class="inline-flex rounded-md shadow-sm" role="group">
 			<button
 				type="button"
-				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+				class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent border bg-slate-100 border-gray-900 rounded-s-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
 				on:click={() => {
 					sol--;
 					fetchData();
 				}}
 			>
+				<svg
+					class="w-4 h-4 text-gray-800 dark:text-white"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="currentColor"
+					viewBox="0 0 10 16"
+				>
+					<path
+						d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z"
+					/>
+				</svg>
 				Previous
 			</button>
 			<button
 				type="button"
-				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white cursor-not-allowed"
+				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white bg-slate-100 border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white cursor-not-allowed"
 			>
 				SOL
 				<span
@@ -75,13 +85,24 @@
 			</button>
 			<button
 				type="button"
-				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+				class="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-transparent bg-slate-100 border border-gray-900 rounded-e-lg hover:bg-gray-900 hover:text-white focus:z-10 focus:ring-2 focus:ring-gray-500 focus:bg-gray-900 focus:text-white dark:border-white dark:text-white dark:hover:text-white dark:hover:bg-gray-700 dark:focus:bg-gray-700"
 				on:click={() => {
 					sol++;
 					fetchData();
 				}}
 			>
 				Next
+				<svg
+					class="w-4 h-4 text-gray-800 dark:text-white"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="currentColor"
+					viewBox="0 0 10 16"
+				>
+					<path
+						d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z"
+					/>
+				</svg>
 			</button>
 		</div>
 	</div>
@@ -102,7 +123,7 @@
 	</div>
 </div>
 
-<div class="grid grid-cols-5 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 gap-4">
+<div class="grid grid-cols-5 md:grid-cols-4 sm:grid-cols-2 gap-4">
 	{#each photos as photo}
 		<figure class="relative max-w-sm cursor-pointer">
 			<a href={photo.img_src}>
