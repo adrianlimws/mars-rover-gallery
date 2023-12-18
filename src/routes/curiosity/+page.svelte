8db932a42c921b4c8f00c3c26b91c088e8550609
Adrian Matsumoto <adrianlimws@gmail.com>
2023-12-18 15:23:07 +1300

    modified search feature

--- src/routes/curiosity/+page.svelte
+++ src/routes/curiosity/+page.svelte
@@ -1,7 +1,11 @@
 <script>
 	import { onMount } from 'svelte';
+	import { writable } from 'svelte/store';
 
-	export let sol = 1;
+	// Create a writable store for sol
+	const solStore = writable(1);
+
+	export let sol;
 	let photos = [];
 
 	// Function to fetch data with the updated sol value
@@ -21,63 +25,109 @@
 		}
 	}
 
-	// Fetch data when the component is mounted or when sol changes
+	// Subscribe to the sol store and fetch data when it changes
+	$: {
+		sol = $solStore;
+		fetchData();
+	}
+
+	// Fetch data when the component is mounted
 	onMount(() => {
 		console.log('Component mounted with sol:', sol);
-		fetchData();
 	});
+
+	// Function to handle input changes and update sol
+	function handleInputChange(event) {
+		const inputValue = parseInt(event.target.value, 10);
+		if (!isNaN(inputValue)) {
+			solStore.set(inputValue);
+		}
+	}
 </script>
 
-<div class="bg-gray-800 p-4">
-	<div class="flex items-center justify-between">
-		<div>
-			<h1 class="text-white">Sol: {sol}</h1>
-		</div>
-		<div class="flex space-x-4">
+<div class="navbar bg-slate-700 text-white">
+	<div class="navbar-start">
+		
+		SOL: {sol}
+	</div>
+	<div class="navbar-center">
+		<div class="inline-flex rounded-md shadow-sm" role="group">
 			<button
-				class="btn"
+				type="button"
+				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
 				on:click={() => {
 					sol--;
 					fetchData();
 				}}
 			>
-				Previous Sol
+				Previous
 			</button>
-
 			<button
-				class="btn"
+				type="button"
+				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border-t border-b border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white cursor-not-allowed"
+			>
+				SOL
+				<span
+					class="inline-flex items-center justify-center w-4 h-4 ms-2 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full"
+				>
+					{sol}
+				</span>
+			</button>
+			<button
+				type="button"
+				class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
 				on:click={() => {
 					sol++;
 					fetchData();
 				}}
 			>
-				Next Sol
+				Next
 			</button>
 		</div>
 	</div>
+	<div class="navbar-end">
+		<form>
+			<label for=""> Search by SOL </label>
+			<input
+				type="number"
+				id="quantity-input"
+				data-input-counter
+				aria-describedby="helper-text-explanation"
+				class="bg-gray-50 border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				bind:value={sol}
+				on:input={handleInputChange}
+				required
+			/>
+		</form>
+	</div>
 </div>
-<div class="grid grid-cols-4 gap-2 p-2">
+
+<div class="grid grid-cols-5 md:grid-cols-4 sm:grid-cols-2 xs:grid-cols-1 gap-4">
 	{#each photos as photo}
 		<figure class="relative max-w-sm cursor-pointer">
-			<a href="#">
+			<a href={photo.img_src}>
 				<img
 					class="rounded-lg w-full"
 					src={photo.img_src}
 					alt={`Image taken by ${photo.rover.name} on SOL ${photo.sol}`}
 				/>
 			</a>
-			<figcaption class="absolute px-4 text-lg text-white bottom-6">
+			<figcaption class="absolute px-2 text-lg text-white bottom-4">
 				<span
-					class="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
+					class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-blue-600"
 					>{photo.earth_date}</span
 				>
 				<span
-					class="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10"
+					class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-red-700"
 					>{photo.rover.name}</span
 				>
 				<span
-					class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20"
+					class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800"
 					>SOL {photo.sol}</span
+				>
+				<span
+					class="inline-flex items-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-purple-800"
+					>CAM: {photo.camera.name}</span
 				>
 			</figcaption>
 		</figure>
